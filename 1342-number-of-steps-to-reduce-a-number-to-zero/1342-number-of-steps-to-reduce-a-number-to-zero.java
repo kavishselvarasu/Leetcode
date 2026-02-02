4ec35dd0fd416f829bf7449e28f69f3c9d9dd871
KAVISH S <kavishselvarasu2007@gmail.com>
2026-02-03 00:00:22 +0530

    0 ms (100%) 42.1 MB (83.69%) .java

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int sum = 0;
+        while(num!=0)
+        {
+            if(num%2==0)
+            {
+                num = num/2;
+            }
+            else
+            {
+                num = num-1;
+            }
+            sum = sum+1;
+        }
+        return sum;
+    }
+}
